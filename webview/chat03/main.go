ad3dc2974ed61395d17b0e83e0a8670256aa7661
yurisa <lcmm5201314@gmail.com>
2024-01-03 00:33:45 +0800

    feat(fyne): modify

--- webview/chat03/main.go
+++ webview/chat03/main.go
@@ -10,7 +10,7 @@ func main() {
 	df := diffmatchpatch.New()
 	r := df.DiffMain(csv1, csv2, false)
 	for _, d := range r {
-		fmt.Printf("type:%s content:%q\n", d.Type, d.Text)
+		fmt.Printf("type:%s content:%q\n\n", d.Type, d.Text)
 	}
 
 	fmt.Println()
@@ -20,13 +20,13 @@ func main() {
 }
 
 var (
-	csv1 = `ID	OnceTicket	OnceDiamond	TenTimesTicket	TenTimesDiamond	LimitCount	ATypeProb	BTypeProb	PRDCount	PRDProb	ATypeFirstCount	ATypeCount	GuaranteePkgId	TargetCount	TargetPkgId	ATypePkg	BTypePkg	CTypePkg	ATypeUpProb	BTypeUpProb	StorageId	ExtraPkgId	GuranteeSR	StartTime	EndTime
+	csv1 = `ID	OnceTicket	OnceDiamond	TenTimes	TenTimesDiamond	LimitCount	ATypeProb	BTypeProb	PRDCount	PRDProb	ATypeCount	GuaranteePkgId	TargetCount	TargetPkgId	ATypePkg	BTypePkg	CTypePkg	ATypeUpProb	BTypeUpProb	StorageId	ExtraPkgId	GuranteeSR	StartTime	EndTime
 1	null	300	null	3000	999	200	1500	51	50	20	100	10009	3	10008	10001	10002	10003	5000	0	1	10099	10006		
 2	null	300	null	3000	999	200	1500	51	50	20	100	10009	3	10008	10001	10002	10003	5000	0	2	10099	10006		
 3	null	300	null	3000	999	200	1500	51	50	20	100	10009	3	10008	10001	10002	10003	5000	0	3	10099	10006		
 4	null	300	null	3000	999	200	1500	51	50	20	100	10009	3	10008	10001	10002	10003	5000	0	4	10099	10006		
 `
-	csv2 = `ID	OnceTicket	OnceDiamond	TenTimesTicket	TenTimesDiamond	LimitCount	ATypeProb	BTypeProb	PRDCount	PRDProb	ATypeFirstCount	ATypeCount	GuaranteePkgId	TargetCount	TargetPkgId	ATypePkg	BTypePkg	CTypePkg	ATypeUpProb	BTypeUpProb	StorageId	ExtraPkgId	GuranteeSR	StartTime	EndTime
+	csv2 = `ID	OnceTicket	OnceDiamond	TenTimesTicket	TenTimesDiamond	LimitCount	ATypeProb	BTypeProb	PRDTimes	AbCProb	ATypeFirstCount	ATypeCount	GuaranteePkgId	TargetCount	TargetPkgId	ATypePkg	BTypePkg	CTypePkg	ATypeUpProb	BTypeUpProb	StorageId	ExtraPkgId	GuranteeSR	StartTime	EndTime
 1	null	300	null	3000	999	200	1500	51	50	20	100	10009	3	10008	10001	10002	10003	5000	0	1	10099	10006		
 2	null	600	null	3000	999	200	1500	51	50	20	100	10009	3	10008	10001	10002	10003	5000	0	2	10099	10006		
 3	null	300	null	8000	999	200	1500	51	50	20	100	10009	3	10008	10001	10002	10003	5000	0	3	10099	10006		
