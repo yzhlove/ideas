ec948cfaff8793308286d9b50816ee8757ad836c
rain <lcmm5201314@gmail.com>
2024-01-05 16:18:55 +0800

    feat(webview): new chat example

--- webview/chat09/main.go
+++ webview/chat09/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -10,8 +12,15 @@ func main() {
 	var a A
 	flag.IntVar(&a.X, "x", 0, "this is version")
 	flag.Parse()
-
 	fmt.Println("a --> ", a)
+
+	if len(os.Args) == 0 {
+		panic("os.Args is nil")
+	}
+	fmt.Println("1 -> ", os.Args[0])
+	fmt.Println(filepath.Abs(os.Args[0]))
+	root, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	fmt.Println(root, err)
 }
 
 type A struct {
